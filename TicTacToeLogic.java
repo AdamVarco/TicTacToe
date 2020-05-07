c53abe4a04301dcd1a29ef5e2b053dc1affae7b6
Adam Varco <adamvarco@live.com>
2020-05-07 11:59:21 -0400

    Update TicTacToeLogic.java

--- TicTacToeLogic.java
+++ TicTacToeLogic.java
@@ -1,98 +1,42 @@
-import java.util.Scanner;
+public class TicTacToeLogic {
+	
+	 private Character winner = null;
 
-public class TicTacToeLogic
-{
+	    public Character getWinner()
+	    {
+	        return this.winner;
+	    }
+	    
+	    public boolean isSolved(char [][] board)
+	    {
+	        this.win(board);
+	        return this.winner != null;
+	    }
 	
-
-	char [][] ticTacToe = new char [3][3];
-	
-	
-	
-	void fillBoard(char [][] board)
-	{
-		for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-            	  ticTacToe[i][j] = board[i][j];
-            }
-    	}
+	boolean win(char board[][]) {
+	     for (int i = 0; i < 3; ++ i) {
+	        if ((board[i][0] != 0) && (board[i][0] == board[i][1]) 
+	                        && (board[i][1] == board[i][2])) {
+	            winner = board[i][0];
+	            return true;
+	        }
+	        if ((board[0][i] != 0) && (board[0][i] == board[1][i]) 
+	                        && (board[1][i] == board[2][i])) {
+	            winner = board[0][i];
+	            return true;
+	        }            
+	    }
+	    if ((board[1][1] != 0) && (board[0][0] == board[1][1]) 
+	                        && (board[1][1] == board[2][2])) {
+	        winner = board[1][1];
+	        return true;
+	    }
+	    if ((board[1][1] != 0) && (board[0][2] == board[1][1]) 
+	                        && (board[1][1] == board[2][0])) {
+	        winner = board[1][1];
+	        return true;
+	    }        
+	    return false;
 	}
-	
-    private Character winner = null;
-
-    public Character getWinner()
-    {
-        return this.winner;
-    }
-
-    public boolean isSolved()
-    {
-    	/*for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-            	ticTacToe1[i][j] = ticTacToe[i][j];
-            }
-    	}*/
-        this.checkSolved();
-        return this.winner != null;
-    }
-
-    private void checkSolved()
-    {
-    	
-        for(int i = 0; i < ticTacToe.length; i++)
-        {
-            Character win = checkRow(i);
-            if(win != null || (win = checkColumn(i)) != null)
-            {
-                this.winner = win;
-                return;
-            }
-        }
-        //Check diagonal top left to bottom right
-        if(ticTacToe[0][0] != ' ')
-        {
-            if(ticTacToe[0][0] == ticTacToe[1][1] &&
-               ticTacToe[1][1] == ticTacToe[2][2])
-            {
-                this.winner = ticTacToe[0][0];
-            }
-        }
-        //Check diagonal top right to bottom left
-        else if(ticTacToe[0][2] != ' ')
-        {
-            if(ticTacToe[0][2] == ticTacToe[1][1] &&
-               ticTacToe[1][1] == ticTacToe[2][0])
-            {
-                this.winner = ticTacToe[0][2];
-            }
-        }
-    }
-
-    private Character checkRow(int row)
-    {
-        if(ticTacToe[row][0] == ' ')
-        {
-            return null;
-        }
-        if(ticTacToe[row][0] == ticTacToe[row][1] &&
-           ticTacToe[row][1] == ticTacToe[row][2])
-        {
-            return ticTacToe[row][0];
-        }
-        return null;
-    }
-
-    private Character checkColumn(int column)
-    {
-        if(ticTacToe[0][column] == ' ')
-        {
-            return null;
-        }
-        if(ticTacToe[0][column] == ticTacToe[1][column] &&
-           ticTacToe[1][column] == ticTacToe[2][column])
-        {
-            return ticTacToe[column][0];
-        }
-        return null;
-    }
-
 }
+
